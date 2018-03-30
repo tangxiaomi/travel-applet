bb2f82e0b5a5b2b057d2db149e5788cbb19e604a
Minmin Tang <minmin.tang@careerbuilder.com>
2018-03-30 15:46:54 +0800

    transplant miniPro to travel-applet

---
+++ pages/tabBar/map/map.js
@@ -0,0 +1,64 @@
+var amapFile = require('../../../libs/amap-wx.js');
+var markersData = [];
+Page({
+  data: {
+    markers: [],
+    latitude: '',
+    longitude: '',
+    textData: {}
+  },
+  makertap: function (e) {
+    var id = e.markerId;
+    var that = this;
+    that.showMarkerInfo(markersData, id);
+    that.changeMarkerColor(markersData, id);
+  },
+  onLoad: function () {
+    var that = this;
+    var myAmapFun = new amapFile.AMapWX({ key: 'c099d1f0cd3330c3c0066fb8f9cac9d4' });
+    myAmapFun.getPoiAround({
+      iconPathSelected: '../../../assets/images/icon-location.png',
+      iconPath: '../../../assets/images/icon-not-marked.png',
+      success: function (data) {
+        markersData = data.markers;
+        that.setData({
+          markers: markersData
+        });
+        that.setData({
+          latitude: markersData[0].latitude
+        });
+        that.setData({
+          longitude: markersData[0].longitude
+        });
+        that.showMarkerInfo(markersData, 0);
+      },
+      fail: function (info) {
+        wx.showModal({ title: info.errMsg })
+      }
+    })
+  },
+  showMarkerInfo: function (data, i) {
+    var that = this;
+    that.setData({
+      textData: {
+        name: data[i].name,
+        desc: data[i].address
+      }
+    });
+  },
+  changeMarkerColor: function (data, i) {
+    var that = this;
+    var markers = [];
+    for (var j = 0; j < data.length; j++) {
+      if (j == i) {
+        data[j].iconPath = "../../../assets/images/icon-location.png"; //如：..­/..­/img/marker_checked.png
+      } else {
+        data[j].iconPath = "../../../assets/images/icon-not-marked.png"; //如：..­/..­/img/marker.png
+      }
+      markers.push(data[j]);
+    }
+    that.setData({
+      markers: markers
+    });
+  }
+})
