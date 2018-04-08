baa0fb1f9953d15429600faa07b7d57c9b265af6
Minmin Tang <minmin.tang@careerbuilder.com>
2018-04-08 18:17:44 +0800

    use qq map

--- pages/tabBar/map/map.js
+++ pages/tabBar/map/map.js
@@ -1,168 +1,47 @@
-var amapFile = require('../../../libs/amap-wx.js');
-var markersData = [];
+var QQMapWX = require('../../../libs/qqmap-wx-jssdk.js');
+
+var qqmapsdk;
+
 Page({
-  data: {
-    markers: [],
-    latitude: '',
-    longitude: '',
-    textData: {},
-    tips: {},
-    src: '',
-  },
-  makertap: function (e) {
-    var id = e.markerId;
-    var that = this;
-    that.showMarkerInfo(markersData, id);
-    that.changeMarkerColor(markersData, id);
-  },
-  onLoad: function (e) {
-    var that = this;
-    var myAmapFun = new amapFile.AMapWX({ key: 'c099d1f0cd3330c3c0066fb8f9cac9d4' });
-    var params = {
-      iconPathSelected: '../../../assets/images/icon-marked.png',
-      iconPath: '../../../assets/images/icon-not-marked.png',
-      success: function (data) {
-        markersData = data.markers;
-        var poisData = data.poisData;
-        var markers_new = [];
-        markersData.forEach(function (item, index) {
-          markers_new.push({
-            id: item.id,
-            latitude: item.latitude,
-            longitude: item.longitude,
-            iconPath: item.iconPath,
-            width: item.width,
-            height: item.height
-          })
 
-        })
-        if (markersData.length > 0) {
-          that.setData({
-            markers: markers_new
-          });
-          that.setData({
-            city: poisData[0].cityname || ''
-          });
-          that.setData({
-            latitude: markersData[0].latitude
-          });
-          that.setData({
-            longitude: markersData[0].longitude
-          });
-          that.showMarkerInfo(markersData, 0);
-        } else {
-          wx.getLocation({
-            type: 'gcj02',
-            success: function (res) {
-              that.setData({
-                latitude: res.latitude
-              });
-              that.setData({
-                longitude: res.longitude
-              });
-              that.setData({
-                city: '北京市'
-              });
-            },
-            fail: function () {
-              that.setData({
-                latitude: 39.909729
-              });
-              that.setData({
-                longitude: 116.398419
-              });
-              that.setData({
-                city: '北京市'
-              });
-            }
-          })
+  onLoad: function () {
 
-          that.setData({
-            textData: {
-              name: '抱歉，未找到结果',
-              desc: ''
-            }
-          });
-        }
+    // 实例化API核心类
 
-      },
-      fail: function (info) {
-        // wx.showModal({title:info.errMsg})
-      }
-    }
-    if (e && e.keywords) {
-      params.querykeywords = e.keywords;
-    }
-    // pol
-    myAmapFun.getPoiAround(params)
-    // ?
-    wx.getSystemInfo({
-      success: function (data) {
-        var height = data.windowHeight;
-        var width = data.windowWidth;
-        var size = width + "*" + height;
-        myAmapFun.getStaticmap({
-          zoom: 8,
-          size: size,
-          scale: 2,
-          markers: "mid,0xFF0000,A:116.37359,39.92437;116.47359,39.92437",
-          success: function (data) {
-            that.setData({
-              src: data.url
-            })
-          },
-          fail: function (info) {
-            wx.showModal({ title: info.errMsg })
-          }
-        })
+    qqmapsdk = new QQMapWX({
 
-      }
-    }),
-    // 逆地址编码 ??
-    // myAmapFun.getRegeo({
-    //   success: function (data) {
-    //     console.log(data)
-    //     //成功回调
-    //   },
-    //   fail: function (info) {
-    //     //失败回调
-    //     console.log(info)
-    //   }
-    // });
-    // 获取天气
-    myAmapFun.getWeather({
-      success: function (data) {
-        console.log(data);
-        //成功回调
-      },
-      fail: function (info) {
-        //失败回调
-        console.log(info)
-      }
-    })
-  },
-  showMarkerInfo: function (data, i) { //每个点的信息
-    var that = this;
-    that.setData({
-      textData: {
-        name: data[i].name,
-        desc: data[i].address
-      }
+      key: '6RSBZ-VYG6Q-MYS54-GACPQ-FWRP3-5SBRQ'
+
     });
+
   },
-  changeMarkerColor: function (data, i) { //点击那些点点触发的逻辑
-    var that = this;
-    var markers = [];
-    for (var j = 0; j < data.length; j++) {
-      if (j == i) {
-        data[j].iconPath = "../../../assets/images/icon-marked.png"; 
-      } else {
-        data[j].iconPath = "../../../assets/images/icon-not-marked.png";
+
+  onShow: function () {
+
+    // 调用接口
+
+    qqmapsdk.search({
+
+      keyword: '彩票',
+
+      success: function (res) {
+
+        console.log(res);
+
+      },
+
+      fail: function (res) {
+
+        console.log(res);
+
+      },
+
+      complete: function (res) {
+
+        console.log(res);
+
       }
-      markers.push(data[j]);
-    }
-    that.setData({
-      markers: markers
+
     });
   }
-})
+  });
--- pages/tabBar/personInfo/personInfo.js
+++ pages/tabBar/personInfo/personInfo.js
@@ -1,4 +1,4 @@
-var amapFile = require('../../../libs/amap-wx.js');
+// var amapFile = require('../../../libs/amap-wx.js');
 Page({
   data: {
     tips: {}
@@ -6,30 +6,30 @@ Page({
   onLoad: function () {
 
   },
-  bindInput: function (e) {
-    var that = this;
-    var keywords = e.detail.value;
-    // var key = config.Config.key;
-    var myAmapFun = new amapFile.AMapWX({ key: 'c099d1f0cd3330c3c0066fb8f9cac9d4' });
-    myAmapFun.getInputtips({
-      keywords: keywords,
-      location: '',
-      success: function (data) {
-        if (data && data.tips) {
-          that.setData({
-            tips: data.tips
-          });
-        }
+  // bindInput: function (e) {
+  //   var that = this;
+  //   var keywords = e.detail.value;
+  //   // var key = config.Config.key;
+  //   var myAmapFun = new amapFile.AMapWX({ key: 'c099d1f0cd3330c3c0066fb8f9cac9d4' });
+  //   myAmapFun.getInputtips({
+  //     keywords: keywords,
+  //     location: '',
+  //     success: function (data) {
+  //       if (data && data.tips) {
+  //         that.setData({
+  //           tips: data.tips
+  //         });
+  //       }
 
-      }
-    })
-  },
-  bindSearch: function (e) {
-    var keywords = e.target.dataset.keywords;
-    var url = '/pages/tabBar/map/map?keywords=' + keywords;
-    console.log(url)
-    wx.redirectTo({
-      url: url
-    })
-  }
+  //     }
+  //   })
+  // },
+  // bindSearch: function (e) {
+  //   var keywords = e.target.dataset.keywords;
+  //   var url = '/pages/tabBar/map/map?keywords=' + keywords;
+  //   console.log(url)
+  //   wx.redirectTo({
+  //     url: url
+  //   })
+  // }
 })
