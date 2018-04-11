5f36a1ff1d057db1589d8be9c91248100773ba0a
Minmin Tang <minmin.tang@careerbuilder.com>
2018-04-11 17:08:45 +0800

    ise

--- pages/tabBar/map/map.js
+++ pages/tabBar/map/map.js
@@ -4,44 +4,21 @@ var qqmapsdk;
 
 Page({
 
-  onLoad: function () {
+  data: {
 
-    // 实例化API核心类
+    resData: []
+
+  },
+
+  onLoad: function (options) {
+
+    // 实例化腾讯地图API核心类
 
     qqmapsdk = new QQMapWX({
 
       key: '6RSBZ-VYG6Q-MYS54-GACPQ-FWRP3-5SBRQ'
-
     });
 
-  },
-
-  onShow: function () {
-
-    // 调用接口
-
-    qqmapsdk.search({
-
-      keyword: '彩票',
-
-      success: function (res) {
-
-        console.log(res);
-
-      },
-
-      fail: function (res) {
-
-        console.log(res);
-
-      },
-
-      complete: function (res) {
-
-        console.log(res);
-
-      }
-
-    });
   }
-  });
+
+})
